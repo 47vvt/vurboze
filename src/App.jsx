f185473ba221871230797aa44ecee5e44fe6cc7c
47vvt <bryansmrtono@gmail.com>
2023-12-12 09:34:45 +1100

    fixed text scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import Session from './pages/Session'
 
 function App() {
 
+
   return (
     <>
+    <div className='relative'>
       <Session/>
+      <div class="h-64 w-full invisible"/>
+    </div>
     </>
   )
 }
